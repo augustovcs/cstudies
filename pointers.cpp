d087c94181fb2d9c219f0f9e3b13f98798d7a1e0
Joao Augusto <augustoviegascs@gmail.com>
2025-11-11 05:59:58 -0300

    studying

--- pointers.cpp
+++ pointers.cpp
@@ -6,9 +6,35 @@ int main() {
     int a = 500;
     int  *p;
 
-    p = &a;
+    int array[5] = {2,4,6,8,10};
+    int *q;
 
-    cout << a;
+
+    // - c lang
+    p = (int *)malloc(5*sizeof(int));
+
+    // - cpp lang
+    //p = new int[5]
+
+    p[0] = 10;
+    p[1] = 15;
+    p[2] = 20;
+    p[3] = 30;
+    p[4] = 50;
+
+    q = array;
+ 
+    cout << *p << endl;
+    cout << *q << endl;
+
+    for (int i = 0; i < 5; i++)
+    cout << p[i] << endl;
+
+    // - cpp lang
+    delete [ ]p;
+    
+    // - c lang
+    //free(p)
 
     return 0;
 

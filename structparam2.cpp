63c1af2c92231a53ffd5564a53d12cbc2e2f7de0
Joao Augusto <augustoviegascs@gmail.com>
2025-11-13 18:16:19 -0300

    added struct more 1 example

--- structparam2.cpp
+++ structparam2.cpp
@@ -8,6 +8,11 @@ struct Rectangle {
     int breadth;
 };
 
+struct test {
+    int A[5];
+    int n;
+};
+
 
 void changeLength(struct Rectangle *p, int l) {
 
@@ -16,13 +21,29 @@ void changeLength(struct Rectangle *p, int l) {
 }
 
 
+void fun(struct test &t1) {
+
+    t1.A[0] = 10;
+    t1.A[1] = 20;
+    t1.A[2] = 30;
+
+    t1.n = 999;
+
+}
+
+
 int main() {
+
+    struct test t={{2,4,6,8,10},5};
+
 
     struct Rectangle r={10, 5};
 
     changeLength(&r, 20);
+    fun(t);
 
     cout<<r.length<<endl;
+    cout<<t.A[0]<<endl;
 
    
 }

de2ec683c1ed6e4b7bdba11d328a3e55cf7f43e9
Joao Augusto <augustoviegascs@gmail.com>
2025-11-20 22:37:16 -0300

    added

---
+++ recursion.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void fun1(int n) {
+	
+if (n > 0) {
+	printf("%d", n);
+	fun1(n-1);	
+	}
+	
+}
+
+
+void fun2(int n) {
+	
+if (n > 0) {
+    fun2(n-1);	
+	printf("%d", n);
+
+	}
+	
+}
+
+int main() {
+
+	int x = 3;
+    fun1(x);
+    cout << "\n"; 
+    fun2(x);
+
+    return 0;
+	
+}
